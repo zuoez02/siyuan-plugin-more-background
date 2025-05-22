64bc30fcdbbf55fdb99bda6e3750e1d23f403dce
Luto Yvan <zuoez02@outlook.com>
2025-05-22 12:39:16 +0000

    v1.0.0: 全新命题，大改

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-# More Background
+# 题头图Plus
 
-随机题头图，点击当前文档空白处，再点击顶栏右上角图标选择类型。
+题头图增强插件，支持多个源的题头图。支持从剪贴板直接导入题头图
 
-## 自定义
+> 预览图中为搭配Asri主题的顶栏融合效果
+
+## 使用方法
+
+1. 安装插件
+2. 无题头图文档点击题头的"添加题头图“
+3. 点击题头图按钮组中的题头图Plus按钮，从按钮中选择一个数据源，或从剪贴板粘贴
+
+## 内置数据源
+
+- ACG
+- Bing今日壁纸
+- Unsplash
+- Picsum
+
+## 自定义数据源
 
 找到工作空间/data/storage/petal/siyuan-plugin-more-background/settings文件，使用记事本打开，按照JSON格式，添加label和url即可。
 
--- index.js
+++ index.js
@@ -1,107 +1,200 @@
-const { Plugin, Menu } = require('siyuan');
+const { Plugin, Menu, showMessage, addIcon } = require('siyuan');
 
-const icon = '<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1928" width="200" height="200"><path d="M1024 910.222222V113.777778c0-62.577778-51.2-113.777778-113.777778-113.777778H113.777778C51.2 0 0 51.2 0 113.777778v796.444444c0 62.577778 51.2 113.777778 113.777778 113.777778h796.444444c62.577778 0 113.777778-51.2 113.777778-113.777778zM312.888889 597.333333l142.222222 170.666667 199.111111-256L910.222222 853.333333H113.777778l199.111111-256z" p-id="1929"></path></svg>';
+const icon = '<symbol id="icon-more-background" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/symbol" p-id="1928" ><path d="M1024 910.222222V113.777778c0-62.577778-51.2-113.777778-113.777778-113.777778H113.777778C51.2 0 0 51.2 0 113.777778v796.444444c0 62.577778 51.2 113.777778 113.777778 113.777778h796.444444c62.577778 0 113.777778-51.2 113.777778-113.777778zM312.888889 597.333333l142.222222 170.666667 199.111111-256L910.222222 853.333333H113.777778l199.111111-256z" p-id="1929"></path></symbol>';
 
 module.exports = class MoreBackgroundPlugin extends Plugin {
     onload() {
-        const c = this.addTopBar({
-            icon,
-            title: '随机题头图',
-            callback: (e) => {
-                this.showThemesMenu(c.getBoundingClientRect())
-            },
-            position: "right"
-        });
-
         this.types = [
             { label: 'ACG', url: 'https://img.xjh.me/random_img.php?return=302' },
+            { label: '必应壁纸', url: 'https://bing.biturl.top/?format=image&index=random&mkt=zh-CN' },
+            { label: 'Unsplash', url: 'https://unsplash.it/1920/1080?random' },
+            { label: 'Picsum', url: 'https://picsum.photos/1920/1080' }
         ];
 
         this.loadStorage();
+
+        this.addIcons(icon);
+
+        this.eventBus.on('loaded-protyle-static', (e) => {
+            const protyle = e.detail.protyle;
+            const background = protyle.background.element;
+            if (!background) {
+                return;
+            }
+            background.addEventListener('mouseover', (event) => {
+                let el = event.target;
+                if (!el.classList.contains('protyle-top')) {
+                    el = el.closest('.protyle-top');
+                }
+                if (!el) {
+                    return;
+                }
+                if (!el.querySelector('.protyle-icons') || el.querySelector('.protyle-icons > .more-background-icon')) {
+                    return;
+                }
+                const firstIcon = el.querySelector('.protyle-icons > .protyle-icon.ariaLabel');
+                if (!firstIcon) {
+                    return;
+                }
+                const newIcon = document.createElement('span');
+                newIcon.classList.add('protyle-icon');
+                newIcon.classList.add('ariaLabel');
+                newIcon.classList.add('more-background-icon');
+                newIcon.setAttribute('data-link', 'more-background');
+                newIcon.setAttribute('aria-label', this.i18n.moreBackgroundBtn);
+                newIcon.innerHTML = '<svg><use xlink:href="#icon-more-background"></use></svg>';
+                firstIcon.before(newIcon);
+                newIcon.addEventListener('click', (e) => {
+                    this.showThemesMenu(e.target.getBoundingClientRect(), protyle)
+                    e.preventDefault();
+                    e.stopImmediatePropagation();
+                })
+            })
+        })
+
+
     }
 
     async loadStorage() {
         const data = await this.loadData('settings');
         if (!data) {
-            this.saveData('settings', JSON.stringify(this.types));
+            return;
         } else {
-            this.types = typeof data === 'string' ? JSON.parse(data) : data;
+            const types = typeof data === 'string' ? JSON.parse(data) : data;
+            this.types = this.types.concat(types);
         }
     }
 
-    showThemesMenu(c) {
-        console.log(this.types)
+    showThemesMenu(c, protyle) {
         const e = new Menu('MoreBackground');
         this.types.forEach((t) => {
             e.addItem({
                 label: t.label,
-                click: () => this.loadRandomBackground(t.label),
+                icon: null,
+                click: () => this.loadRandomBackground(t.label, protyle),
             })
         })
+        e.addSeparator();
+        e.addItem({
+            label: this.i18n.uploadFromClipboard,
+            icon: 'iconClipboard',
+            click: () => this.uploadFromClipboard(protyle),
+        });
         e.open({ x: c.left, y: c.bottom, isLeft: !1 })
     }
 
-    loadRandomBackground(label) {
+    loadRandomBackground(label, protyle) {
         const url = this.types.find((t) => t.label === label).url;
-        triggerRandomIfNoImg();
-        this.loadImgSetHeader(url);
+        const el = protyle.contentElement;
+        triggerRandomIfNoImg(el);
+        this.loadImgSetHeader(url, protyle);
+        this.lastType = label;
     }
 
-    async loadImgSetHeader(url) {
-        const currentId = getFileID();
-        const headers = document.querySelectorAll(`.protyle-background[data-node-id="${currentId}"] div.protyle-background__img img`)
-        const img = await fetch(url)
-        const imgurl = img.url
-        headers.forEach(
-            el => {
-                el.setAttribute("style", "")
-                el.setAttribute("src", imgurl)
+    async uploadFromClipboard(protyle) {
+        let img;
+        const clipboardItems = await navigator.clipboard.read();
+        for (const clipboardItem of clipboardItems) {
+            if (clipboardItem.types.includes("image/png")) {
+                const blob = await clipboardItem.getType("image/png");
+                const imageURL = URL.createObjectURL(blob);
+                img = imageURL;
+                break;
             }
-        )
-        fetch('/api/attr/setBlockAttrs', {
-            method: 'post',
-            body: JSON.stringify({
-                id: currentId,
-                attrs: {
-                    "title-img": `background-image:url(${imgurl})`
+        }
+        if (!img) {
+            showMessage(this.i18n.noImageInClipboard);
+            return;
+        }
+        const el = protyle.contentElement;
+        triggerRandomIfNoImg(el);
+        this.loadImgBlobSetHeader(img, protyle);
+    }
+
+    async loadImgBlobSetHeader(url, protyle) {
+        const id = protyle.background.ial.id;
+        protyle.background.element.setAttribute('style', 'cursor: wait');
+        let img;
+        try {
+            img = await fetch(url)
+        } catch {
+            showMessage(this.i18n.loadUrlFailed)
+            protyle.background.element.setAttribute('style', 'cursor: wait');
+            return;
+        }
+        const imgurl = img.url
+        try {
+            const res = await fetch(imgurl);
+            const blob = await res.blob();
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            await new Promise((resolve, reject) => {
+                reader.onloadend = () => {
+                    const base64data = reader.result;
+                    fetch('/api/attr/setBlockAttrs', {
+                        method: 'post',
+                        body: JSON.stringify({
+                            id: id,
+                            attrs: {
+                                "title-img": `background-image:url(${base64data})`
+                            }
+                        })
+                    }).finally(() => {
+                        resolve();
+                    })
                 }
-            })
-        })
+                reader.onerror = reject;
+                reader.onabort = reject;
+            });
+        } finally {
+            protyle.background.element.removeAttribute('style');
+        }
+    }
+
+    async loadImgSetHeader(url, protyle) {
+        const id = protyle.background.ial.id;
+        protyle.background.element.setAttribute('style', 'cursor: wait');
+        let img;
+        try {
+            img = await fetch(url)
+        } catch {
+            showMessage(this.i18n.loadUrlFailed)
+            protyle.background.element.setAttribute('style', 'cursor: wait');
+            return;
+        }
+        const imgurl = img.url
+        try {
+            const res = await fetch(imgurl);
+            const blob = await res.blob();
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            await new Promise((resolve, reject) => {
+                reader.onloadend = () => {
+                    const base64data = reader.result;
+                    fetch('/api/attr/setBlockAttrs', {
+                        method: 'post',
+                        body: JSON.stringify({
+                            id: id,
+                            attrs: {
+                                "title-img": `background-image:url(${base64data})`
+                            }
+                        })
+                    }).finally(() => {
+                        resolve();
+                    })
+                }
+                reader.onerror = reject;
+                reader.onabort = reject;
+            });
+        } finally {
+            protyle.background.element.removeAttribute('style');
+        }
     }
 
 }
 
-function getFileID() {
-    //获取当前页面
-    const currentPage = getCurrentPage();
-    //获取当前页面id
-    const currentPageID = currentPage.querySelector(
-        "span.protyle-breadcrumb__item--active"
-    ).getAttribute("data-node-id");
-
-    return currentPageID;
-}
-
-function triggerRandomIfNoImg() {
-    const currentPage = getCurrentPage();
+function triggerRandomIfNoImg(currentPage) {
     currentPage.querySelector('.protyle-background__img > img.fn__none')?.classList.remove('fn__none');
     currentPage.querySelector('.protyle-background')?.setAttribute('style', 'min-height: 30vh;');
     currentPage.querySelector('.protyle-background__img > .protyle-icons > span[data-type="position"]')?.classList.remove('fn__none');
-}
-
-function getCurrentPage() {
-    var currentScreen;
-    var currentPage;
-    try {
-        //获取当前屏幕
-        currentScreen = document.querySelector(".layout__wnd--active");
-        //获取当前页面
-        currentPage = currentScreen.querySelector(
-            ".fn__flex-1.protyle:not(.fn__none)"
-        );
-        return currentPage;
-    } catch (e) {
-        console.error(`未能获取到页面焦点！`)
-    }
-    throw new Error("未能获取到页面焦点！");
 }
